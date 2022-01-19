554b0444bdbc8119a96530eebd52213c2fa9e2dc
tod619 <trevorodwyer619@hotmail.com>
2022-01-19 20:41:13 +0000

    create functions resetSelected + select in script.js file

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const computerScoreEl = document.getElementById('computer-score')
 const computerChoiceEl = document.getElementById('computer-choice')
 const resultText = document.getElementById('resultText')
 
-const playerRock = documet.getElementById('playerRock')
+const playerRock = document.getElementById('playerRock')
 const playerPaper =  document.getElementById('playerPaper')
 const playerScissors = document.getElementById('playerScissors')
 const playerLizard = document.getElementById('playerLizard')
 const playerSpock = document.getElementById('playerSpock')
 
-const computerRock = documet.getElementById('computerRock')
+const computerRock = document.getElementById('computerRock')
 const computerPaper =  document.getElementById('computerPaper')
 const computerScissors = document.getElementById('computerScissors')
 const computerLizard = document.getElementById('computerLizard')
@@ -26,3 +26,46 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
+// Reset all selected icons
+function resetSelected() {
+  allGameIcons.forEach((icon) => {
+    icon.classList.remove('selected')
+  })
+}
+
+// passing player selection value and styling icon
+function select(playerChoice) {
+  resetSelected()
+  // Add 'selected' styling & playerChoice
+  switch(playerChoice) {
+    case 'rock':
+      playerRock.classList.add('selected')
+      playerChoiceEl.textContent = ' --- Rock'
+      break
+
+      case 'paper':
+      playerPaper.classList.add('selected')
+      playerChoiceEl.textContent = ' --- Paper'
+      break
+
+      case 'scissors':
+      playerScissors.classList.add('selected')
+      playerChoiceEl.textContent = ' --- scissors'
+      break
+
+      case 'lizard':
+      playerLizard.classList.add('selected')
+      playerChoiceEl.textContent = ' --- Lizard'
+      break
+
+      case 'spock':
+      playerSpock.classList.add('selected')
+      playerChoiceEl.textContent = ' --- Spock'
+      break
+
+      default:
+        break
+
+
+  }
+}
