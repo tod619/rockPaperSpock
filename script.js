6923b7f22cc272c55776cad7b65c41d9ea4fb224
tod619 <trevorodwyer619@hotmail.com>
2022-01-19 20:15:10 +0000

    create game variables in script.js file

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+const playerScoreEl = document.getElementById('player-score')
+const playerChoiceEl = document.getElementById('player-choice')
+const computerScoreEl = document.getElementById('computer-score')
+const computerChoiceEl = document.getElementById('computer-choice')
+const resultText = document.getElementById('resultText')
 
+const playerRock = documet.getElementById('playerRock')
+const playerPaper =  document.getElementById('playerPaper')
+const playerScissors = document.getElementById('playerScissors')
+const playerLizard = document.getElementById('playerLizard')
+const playerSpock = document.getElementById('playerSpock')
+
+const computerRock = documet.getElementById('computerRock')
+const computerPaper =  document.getElementById('computerPaper')
+const computerScissors = document.getElementById('computerScissors')
+const computerLizard = document.getElementById('computerLizard')
+const computerSpock = document.getElementById('computerSpock')
+
+const allGameIcons = document.querySelectorAll('.far')
 
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
